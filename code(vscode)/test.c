df75681e6e0fab3c15d11bb0b128a7de307acfad
crab-s <1659905380@qq.com>
2025-10-04 18:17:26 +0800

    Add files via upload

---
+++ code(vscode)/test.c
@@ -0,0 +1,975 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//     printf("hello world");
+//     return 0;
+// }
+// #include <stdio.h>
+//
+// int main()
+//{
+//	printf("%f",3.1415);
+//  printf("%lf",3.1415) ;                                       打印小数，默认小数点后六位
+//	return 0;
+// }
+// int main()
+//{
+//	printf("%d", 10);                                           打印整数
+//	return 0;
+// }
+// int main()
+//{
+//	printf("%c", 'x');											/*打印字符*/
+//	return 0;
+// }
+// stdio
+// standard-标准
+// input-输入
+// output-输出
+// #include <stdio.h>
+// int main()
+//{
+//	printf("hello world\n");
+//	return 0;
+// }
+// #include <stdio.h>
+//
+// int main()
+//{
+//	printf("%c\n", 'a'-32);
+//	return 0;
+// }
+// #include <stdio.h>
+//
+// int main(
+//{
+//	printf("%d\n", '\n');
+//	return 0;
+// }
+// #include <stdio.h>
+//
+// int main()
+//{
+//	printf("abcdef\n");											\n:换行符
+//	printf("123");
+//	return 0;
+// }
+// #include <stdio.h>
+//
+// int main()
+//{
+//	int i = 0;
+//	for (i = 32; i <= 127; i++)
+//	{
+//		printf("%c", i);
+//		if (i % 16 == 15)
+//			printf("\n");
+//	}
+//
+//	return 0;
+// }
+// #include <stdio.h>
+//
+// int main()
+//{
+//		printf("%s\n", "hello world");
+//		printf("hello world");
+//		return 0;
+// }
+// #include <stdio.h>
+//
+// int main()
+//{
+//	"abcdef";
+//	return 0;
+// }
+// #include <stdio.h>
+//
+// int main()
+//{
+//	char arr1[] = { 'a','b','c','\0'};
+//	char arr2[] = "abc";
+//	printf("%s\n", arr1);
+//	printf("%s", arr2);
+//	return 0;
+// }
+// #include <stdio.h>
+//
+// int main()
+//{
+//	printf("%d\n", 8.0/5.0);
+//	return 0;
+// }
+
+// #include <stdio.h>
+// #include <math.h>
+//
+// int main()
+//{
+//	printf("%.1f", sqrt(9.0));
+//	return 0;
+// }
+// #include <stdio.h>
+//
+// int main()
+//{
+//	int a=5;
+//	printf("%p\n", &a);
+//	return 0;
+// }
+// #include <stdio.h>
+//
+// int main()
+//{
+//	int a, b, c;
+//	scanf("%d %d %d", &a, &b, &c);
+//	int count = 0;
+//	if (a < 60)
+//		count++;
+//	if (b < 60)
+//		count++;
+//	if (c < 60);
+//		count++;
+//	if (count == 1)
+//		printf("1");
+//	else
+//		printf("0");
+//	return 0;
+//
+// }
+// #include <stdio.h>
+//
+// int main()
+//{
+//	char arr1[] = { 'a','b','c','\0' };
+//	char arr2[] = "abc" ;
+//	printf("%s\n", arr1);
+//	printf("%s", arr2);
+//	return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+//{
+//	printf("abc\\nd");
+//	return 0;
+// }
+// #include <stdio.h>
+//
+// int main()
+//{
+//	printf("%s","a\'");
+//	return 0;
+// }
+// #include <stdio.h>
+//
+// int main()
+//{
+//	printf("%s\n","abc");
+//	printf("%s", "abc\r");
+//	return 0;
+// }
+// int main()
+//{
+//	printf("%s\n","abc\vdef");
+//	return 0;
+// }
+// #include <stdio.h>
+//
+// int main()
+//{
+//	printf("%c\n", '\130');
+//	printf("%c", '\x35');
+//	return 0;
+// }
+// #include <stdio.h>
+//;
+//
+// int main()
+//{
+//	char arr1[] = { 'a','b','c','\0' };
+//	char arr2[] = "abcd";
+//	printf("%s\n", arr1);
+//	printf("%s", arr2);
+//	return 0;
+// }
+// #include <stdio.h>
+//
+// int main()
+//{
+//	int a, b;
+//	scanf("%d %d", &a, &b);
+//	printf("%d\n", a);
+//	printf("%d", b);
+//	return 0;
+// }
+// #include <stdio.h.>
+//
+// int main()
+//{
+//	printf("hello world");
+//	return 0;
+// }
+////#include <stdio.h>
+//
+// int main()
+//{
+//	printf("hello world\n");
+//	return 0;
+//}
+// #include <stdio.h>
+//
+// int main()
+//{
+//	char arr1[] = { 'a','b','c','\0' };
+//	char arr2[] = "abc";
+//	printf("%s\n", arr1);
+//	printf("%s", arr2);
+//	return 0;
+//}
+// #include <stdio.h>
+// #include <string.h>
+//
+// int main()
+//{
+//	int len = strlen("abcde");
+//	printf("%d\n", len);
+//	return 0;
+//}\
+//
+// #include <stdio.h>
+// #include <string.h>
+//
+// int main()
+//{
+//	int len = strlen("c:\test\129\test.c");
+//	printf("%d\n", len);
+//	return 0;
+//}
+// #include <stdio.h>
+
+// int main()
+//{
+//	printf("%c",'\135');
+//	return 0;
+// }
+// #include <stdio.h>
+// #include <string.h>
+//
+// int main()
+//{
+//	int len = strlen("c:\test\129\test.c");
+//	printf("%d\n", len);
+//	return 0;
+// }
+// #include <stdio.h>
+//
+// int main()
+//{
+//	;
+//	return 0;
+// }
+// #include <stdio.h>
+// #include <math.h>
+//
+// int main()
+//{
+//	printf("%.8f\n", 1 + 2 * sqrt(3) / (5 - 0.1));
+//	return 0;"
+// }
+// #include <stdio.h>
+//
+// int main()
+//
+//{
+//	int a, b;
+//	scanf("%d %d", &a, &b);
+//	printf("%d\n", a + b);
+//	return 0;
+// }
+// #include <stdio.h>
+//
+// int main()
+//{
+//	char arr1[] = { 'a','b','c','\0'};
+//	char arr2[] = "abc";
+//	printf("%s\n", arr1);
+//	printf("%s", arr2);
+//	return 0;
+// }
+// #include <stdio.h>
+//
+// int main()
+//{
+//     int sum = 0;
+//
+//     for (int i = 1; i <= 100; i++)
+//     {
+//         sum += i;
+//     }
+//
+//     printf("1到100的和为：%d\n", sum);
+//     return 0;
+// }
+// #include <stdio.h>
+//
+// int main()
+//{
+//	int n = 100;
+//	int sum = (n + 1) * n / 2;
+//	printf("结果为：%d\n", sum);
+//	return 0;
+// }
+
+// #include <stdio.h>
+// #include <stdbool.h>
+//
+// int main()
+//{
+//	bool flag = false;
+//	if (flag)
+//	printf("i like C\n");
+//	return 0;
+// }
+// #include <stdio.h>
+//
+// int main()
+//{
+//	printf("%zd\n", sizeof(_Bool));			//1
+//	printf("%zd\n", sizeof(char));			//1
+//	printf("%zd\n", sizeof(short));			//2
+//	printf("%zd\n", sizeof(int));			//4
+//	printf("%zd\n", sizeof(long));			//4
+//	printf("%zd\n", sizeof(long long));		//8
+//	printf("%zd\n", sizeof(float));			//4
+//	printf("%zd\n", sizeof(double));		//8
+//	printf("%zd\n", sizeof(long double));	//8
+//	return 0;
+// }
+// #include <stdio.h>
+//
+// int main()
+//{
+//	int num = 100;
+//	printf("%zd\n", sizeof num);
+//	printf("%zd\n", sizeof(num));
+//	printf("%zd\n", sizeof(int));
+//	return 0;
+// }
+//
+// #include <stdio.h>
+//
+// int main()
+//{
+//	short s = 10;
+//	int num = 100;
+//	printf("%zd\n", sizeof(s = num+2));
+//	printf("%d\n", s);
+//	return 0;
+// }
+// #include <stdio.h>
+//
+// int main()
+//{
+//	int num = 10;
+//	num = -10;
+//	unsigned num2 = 100;
+//	num2 = -100;
+//	printf("%d\n", num);
+//	printf("%u", num2);
+//	return 0;
+// }
+
+// #include <stdio.h>
+// #include <stdbool.h>
+//
+// int main()
+//{
+//	 _Bool flag = false;
+//	if (flag)
+//		printf("123");
+//	return 0;
+// }
+// #include  <stdio.h>
+// #include <limits.h>
+//
+// int main()
+//{
+//	printf("%d\n", INT_MAX);
+//	return 0;
+// }
+// #include <stdio.h>
+//
+// int main()
+//{
+//	printf("%zd", sizeof(3+3.5));
+//	return 0;
+// }
+// #include <stdio.h>
+//
+// int global = 2025;
+// int main()
+//{
+//	int local = 2026;
+//	printf("%d\n", global);
+//	printf("%d\n", local);
+//
+//	return 0;
+// }
+// #include <stdio.h>
+//
+// int n = 1000;
+//
+// int main()
+//{
+//	int n = 100;
+//	printf("%d\n", n);
+//	return 0;
+// }
+// #include <stdio.h>
+//
+// int main()
+//{
+//	int i = 1;
+//	i += 1;
+//	printf("%d\n", i);
+//	return 0;
+// }
+// #include <stdio.h>
+//
+// int main()
+//{
+//	int a, b;
+//	scanf("%d %d", &a, &b);
+//	printf("%d\n", a + b);
+//	return 0;
+// }
+// #include <stdio.h>
+//
+// int main()
+//{
+//	int y = 10 / 6;
+//	double x = 10.0 / 6.0;
+//	printf("%.8f\n", x);
+//	printf("%d\n", y);
+//	return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     float f = 123.456f;
+//     printf("%e\n", f);
+//     return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     int a = 2;
+//     int b = 3;
+//     int c = 4;
+//     b = ++a,c++;
+//     printf("%d\n", b);
+//     return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     int score;
+//     printf("please input your score:");
+//     scanf("%d", &score);
+//     printf("your score is:%d\n", score);
+//     return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     int x;
+//     float y;
+//     scanf("%d", &x);
+//     printf("%d\n", x);
+//     scanf("%f", &y);
+//     printf("%f\n", y);
+//     return 0;
+// }
+// #include  <stdio.h>
+
+// int main()
+// {
+//     int a = 0;
+//     int b = 0;
+//     int c = 0;
+//     int r = scanf("%d %d %d", &a, &b, &c);
+//     printf("r=%d\n", r);
+//     printf("a=%d,b=%d,c=%d", a, b, c);
+//     return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     int a;
+//     int r = scanf("%d", a);
+//     printf("%d\n", r);
+//     return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     int a;
+//     int b;
+//     while (scanf("%d %d",&a,&b) !=EOF)
+//     {
+//         printf("%d", a + b);
+//     }
+//     return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i = 0;
+//     for (i=1; i<=100; i++ )
+//     {
+//         if(i%2==0)
+//             printf("%d ", i);
+//     }
+//         return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i = 0;
+//     for (i = 1; i <= 100;i+=3)
+//     {
+//             printf("%d ", i);
+//     }
+//     return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i = 0;
+//     int j = 0;
+//     for (i = 1; i <= 9;i+=1)
+//     {
+//         for (j = 1; j <= i;j+=1)
+//             {
+//                 printf("%d*%d=%-3d", i,j,i*j);
+//             }
+//             printf("\n");
+//     }
+//     return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i = 0;
+//     for (i = 1; i <= 100;i++)
+//     {
+//         if(i%2==1)
+//             printf("%d ", i);
+//     }
+//         return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i = 0;
+//     int j = 0;
+//     for (i = 1; i <= 9;i++)
+//     {
+//         for (j = 1; j <= i;j++)
+//         printf("%d*%d=%-4d", i, j, i * j);
+//         printf("\n");
+//     }
+//         return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     char ch = 0;
+//     scanf(" %c", &ch);
+//     printf("%c", ch);
+//     return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     char name[2];                              通过scanf输入的字符数量应该比数组的总字符数量少一以便为\0留出一个位置
+//     scanf("%1s", name);
+//     printf("%s\n", name);
+//     return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     int year;
+//     int month;
+//     int day;
+//     scanf("%d%*c%d%*c%d", &year, &month, &day);
+//     printf("%d %d %d\n", year, month, day);
+//     return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     int x, y, z;
+//     scanf("%d %d %d", &x, &y, &z);
+//     printf("%d\n%d\n%d", x, y, z);
+//     return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i = 0;
+//     scanf("%d", &i);
+//     if (i % 2 == 1)
+//         printf("i is odd number");
+//     else
+//         printf("i is even number");
+//         return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     int num = 0;
+//     scanf("%d", &num);
+//     if(num>=18)
+//     {
+//         printf("adult\n");
+//         printf("you are welcome!");
+//     }
+//     else
+//         printf("nonage");
+//     return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     int num = 0;
+//     scanf("%d", &num);
+//     if(num==0)
+//         printf("num is 0");
+//         else if (num > 0)
+//             printf("num is positive");
+//         else
+//             printf("num is negative");
+//     return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     int age = 0;
+//     scanf("%d", &age);
+//     if (age < 18)
+//         printf("nonage");
+//     else if(age <= 44)
+//         printf("teenager");
+//     else if(age <= 59)
+//         printf("mid age");
+//     else if(age <= 89)
+//         printf("the olds");
+//     else
+//         printf("very old people");
+//     return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     int a, b, c;
+//     scanf("%d %d %d", &a, &b, &c);
+//     if(a + b> c && b + c> a && a + c> b)
+//     {
+//         if(a == b&& b == c)
+//             printf("equilteral triangle");
+//         else if(a==b||b==c||a==c)
+//             printf("isosceles triangle");
+//         else if(a*a+b*b==c*c||a*a+c*c==b*b||c*c+b*b==a*a)
+//             printf("right triangle");
+//         else
+//             printf("general triangle");
+//     }
+//     else
+//         printf("error");
+//     return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     printf("hello world");
+//     return 0;
+// }
+// #include <stdio.h>
+// #include <stdbool.h>
+
+// int main()
+// {
+//     bool flag = true;
+//     bool app = false;
+//     printf("%d\n", flag);
+//     printf("%d", app);
+//     return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     int a = 1;
+//     int b = 2;
+//     int c;
+//     c = a + b;
+//     printf("%d", c);
+//     return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     int year = 0;
+//     scanf("%d", &year);
+//     if (year >= 18 && year <= 36)
+//         printf("teenager");
+//     else
+//         printf("error");
+//         return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     int a = 0;
+//     int b = 0;
+//     scanf("%d %d", &a, &b);
+//     int m = a > b ? a : b;
+//     printf("%d\n", m);
+//     return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     int flag = 0;
+//     scanf("%d", &flag);
+//     if (!flag)
+//         printf("hehe\n");
+//     return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     int year = 0;
+//     scanf("%d", &year);
+//     if ((year % 4 == 0 && year % 100 != 0) || year % 100 != 0)
+//         printf("target year");
+//     else
+//         printf("invalid year");
+//     return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     int year = 0;
+//     scanf("%d", &year);
+//     if (year % 4 == 0 && year % 100 != 0)
+//         printf("target year");
+//     else if (year % 400 == 0)
+//         printf("target year");
+//     else
+//         printf("invalid year");
+//     return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i = 0, a = 0, b = 2, c = 3, d = 4;
+//     i = a++ || ++b || d++;
+//     printf("%d\n%d\n%d\n%d\n%d", i, a, b, c, d);
+//     return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i = 0;
+//     scanf("%d", &i);
+//     switch ( i % 3 )
+//     {
+//     case 0:
+//         printf("int");
+//         break;
+//     case 1:
+//         printf("1");
+//         break;
+//     case 2:
+//         printf("2");
+//         break;
+//     }
+
+//     return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     int day = 0;
+//     scanf("%d", &day);
+//     switch ( day )
+//     {
+//         case 1:
+//         case 2:
+//         case 3:
+//         case 4:
+//         case 5:
+//             printf("work");
+//             break;
+//         case 6:
+//         case 7:
+//             printf("break");
+//             break;
+//         default:
+//             printf("error");
+//     }
+//     return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     int day = 0;
+//     scanf("%d", &day);
+//     int a = day % 7;
+//     printf("%d", a);
+//     return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i = 1;
+//     while (i <= 10)
+//     {
+//         printf("%d\n", i);
+//         i++;
+//     }
+//         return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i = 0;
+//     scanf("%d", &i);
+//     while ( i > 0 )
+//     {
+//         int m;
+//         m = i % 10;
+//         printf("%d\n", m);
+//         i = i / 10;
+//     }
+//     return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i = 0;
+//     scanf("%d", &i);
+//     while (i)
+//     {
+//         printf("%d\n", i % 10);
+//         i /= 10;
+//     }
+//     return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i = 0;
+//     scanf("%d", &i);
+//     while ( i > 0)
+//     {
+//         printf("%d\n", i % 10);
+//         i /= 10;
+//     }
+//     return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i = 0;
+//     scanf("%d", &i);
+//     switch ( i % 3 )
+//     {
+//         case 1:
+//             printf("%d", 1);
+//             break;
+//         case 2:
+//             printf("%d", 2);
+//             break;
+//         default :
+//             printf("error");
+//     }
+//     return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i = 0;
+//     for (i = 100; i <= 200; i++)
+//     {
+//         int j = 0;
+//         int m = 1;
+//         for (j = 2; j < i; j++)
+//         {
+//             if (i % j == 0)
+//             {
+//                 m = 0;
+//                 break;
+//             }
+//         }
+//         if (m == 1)
+//             printf("%d ", i);
+//     }
+//         return 0;
+// }                                                打印100到200之间的素数
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i = 0;
+//     int j = 0;
+//     for (i = 1; i <= 9; i ++)
+//     {
+//         for (j = 1; j <= i; j ++)
+//             printf("%d*%d=%-3d", i, j, i * j);
+//             printf("\n");
+//     }
+//         return 0;
+// }
+//
